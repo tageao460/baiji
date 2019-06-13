b1d15441a2cd62abeac76a0caa52fb52c895850c
kevin.zwy <kevin.zwy@alibaba-inc.com>
2019-06-13 21:21:56 +0800

    update

--- attack.py
+++ attack.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 from tensorflow.contrib.slim.nets import inception
 from scipy.misc import imread
 from scipy.misc import imresize
-from cleverhans.attacks import BasicIterativeMethod, Attack
+from cleverhans.attacks import BasicIterativeMethod, Attack, FastGradientMethod
 from cleverhans.attacks import Model
 from PIL import Image
 
@@ -110,8 +110,8 @@ def main(_):
         model = InceptionModel(nb_classes)
         # Run computation
         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
-            fgsm_model = Attack(model, sess=sess)
-            #attack_params = {"eps":32.0 / 255.0, "clip_min": -1.0, "clip_max": 1.0}
+            fgsm_model = FastGradientMethod(model, sess=sess)
+            attack_params = {"eps":32.0 / 255.0, "clip_min": -1.0, "clip_max": 1.0}
             x_adv = fgsm_model.generate(x_input, **attack_params)
             saver = tf.train.Saver(slim.get_model_variables())
             saver.restore(sess, FLAGS.checkpoint_path)
