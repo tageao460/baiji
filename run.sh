f2c85ab16a31aefc1d4982f32bcb2b8d4671c756
周芹 <xining.zq@IT-C02YF226JHD2.local>
2019-06-14 07:46:15 +0800

    vgg16

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@ OUTPUT_DIR=$2
 python attack.py \
   --input_dir="${INPUT_DIR}" \
   --output_dir="${OUTPUT_DIR}" \
-  --checkpoint_path=./models/vgg/vgg_16.ckpt
+  --checkpoint_path=./models/vgg_16/vgg_16.ckpt
