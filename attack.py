1f8e13850f0dee3bb8631aae4ced78dac0b737cd
bella <bella@IT-C02YD3F0JHD2.local>
2019-06-13 20:13:58 +0800

    add model3

--- attack.py
+++ attack.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 from tensorflow.contrib.slim.nets import inception
 from scipy.misc import imread
 from scipy.misc import imresize
-from cleverhans.attacks import clip_eta
+from cleverhans.attacks import fgm
 from cleverhans.attacks import Model
 from PIL import Image
 
@@ -111,7 +111,7 @@ def main(_):
         model = InceptionModel(nb_classes)
         # Run computation
         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
-            fgsm_model = clip_eta(model, sess=sess)
+            fgsm_model = fgm(model, sess=sess)
             attack_params = {"eps":32.0 / 255.0, "clip_min": -1.0, "clip_max": 1.0}
             x_adv = fgsm_model.generate(x_input, **attack_params)
             saver = tf.train.Saver(slim.get_model_variables())
