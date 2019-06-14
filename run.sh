1481e4c923dfc709704b18d1d5eddd7a429bfc96
周芹 <xining.zq@IT-C02YF226JHD2.local>
2019-06-14 12:44:24 +0800

    v4

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@ sh merge inception_v3.ckpt
 python attack.py \
   --input_dir="${INPUT_DIR}" \
   --output_dir="${OUTPUT_DIR}" \
-  --checkpoint_path=./inception_v3.ckpt
+  --checkpoint_path=inception_v3.ckpt
